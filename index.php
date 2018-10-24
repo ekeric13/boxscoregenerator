9daf4ad6ab658e8d16bfd460ae2542296dc660ca
ekeric13 <ekeric13@gmail.com>
2018-10-23 23:06:21 -0700

    Try again

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if ($ready) {
    'GameID'  => $gameID
   ));
   curl_setopt($ch, CURLOPT_URL, "https://stats.nba.com/stats/boxscoresummaryv2?".$data);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $stat_headers);
+  // curl_setopt($ch, CURLOPT_HTTPHEADER, $stat_headers);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_ENCODING, "");
   curl_setopt($ch, CURLOPT_VERBOSE, true);
